1dcd4f97552e282e43a08539db4929d02cac4797
Natblow <natblow@hotmail.fr>
2022-10-14 21:57:03 -0400

    set routes for line item dates

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :quotes
+  resources :quotes do
+    resources :line_item_dates, except: [:index, :show]
+  end
   root "pages#home"
 end
